757696e8c85aadb908b882c67012d603f3741d3c
Tokageki <58886109+tokageki@users.noreply.github.com>
2021-03-04 07:10:17 +0100

    Add files via upload

---
+++ lalie/app.js
@@ -0,0 +1,15 @@
+const sr = ScrollReveal();
+
+sr.reveal('.youWant',{
+    duration:2000,
+})
+
+sr.reveal('.picReset',{
+    duration:2000,
+    delay:500,
+})
+
+sr.reveal('.texteUn',{
+    duration:2000,
+    delay:200,
+})
