1a39dd1a37a319e72e5bc3e8048c369f724cbcef
Tokageki <58886109+tokageki@users.noreply.github.com>
2021-03-11 14:26:33 +0100

    là cette fois c'est la bonne version xD

---
+++ lalie/scripts/app.js
@@ -0,0 +1,15 @@
+const sr = ScrollReveal();
+
+sr.reveal('.youWant',{
+    duration:2000,
+})
+
+sr.reveal('.imageUn',{
+    duration:2000,
+    delay:500,
+})
+
+sr.reveal('.texte',{
+    duration:2000,
+    delay:400,
+})
